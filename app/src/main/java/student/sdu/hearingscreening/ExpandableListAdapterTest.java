a89c10be87bd0c0c98031a8484e0ad5b1daa2199
MegaCakeEater <emil_ngaatd@hotmail.com>
2017-02-13 10:21:56 +0100

    Vi prøver igen

--- app/src/main/java/student/sdu/hearingscreening/ExpandableListAdapterTest.java
+++ app/src/main/java/student/sdu/hearingscreening/ExpandableListAdapterTest.java
@@ -1,8 +1,83 @@
 package student.sdu.hearingscreening;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
 /**
  * Created by Bogs on 08-02-2017.
  */
 
-public class ExpandableListAdapterTest {
+public class ExpandableListAdapterTest extends BaseExpandableListAdapter {
+    private Context context;
+    private String[][] contents;
+    private String[] tests;
+
+    public ExpandableListAdapterTest(Context context, String[][] contents, String[] tests) {
+        super();
+        this.context = context;
+        this.contents = contents;
+        this.tests = tests;
+    }
+
+    @Override
+    public String getChild(int groupPosition, int childPosition) {
+        return contents[groupPosition][childPosition];
+    }
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+    @Override
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        TextView row = (TextView)convertView;
+        if(row == null) {
+            row = new TextView(context);
+        }
+        row.setText(contents[groupPosition][childPosition]);
+        return row;
+    }
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return contents[groupPosition].length;
+    }
+    @Override
+    public String[] getGroup(int groupPosition) {
+        return contents[groupPosition];
+    }
+    @Override
+    public int getGroupCount() {
+        return contents.length;
+    }
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        TextView row = (TextView)convertView;
+        if(row == null) {
+            row = new TextView(context);
+        }
+        row.setTypeface(Typeface.DEFAULT_BOLD);
+        row.setText(tests[groupPosition]);
+        return row;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
 }
--- app/src/main/java/student/sdu/hearingscreening/ResultsActivity.java
+++ app/src/main/java/student/sdu/hearingscreening/ResultsActivity.java
@@ -1,7 +1,11 @@
 package student.sdu.hearingscreening;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        setupTextViews();
+        setupButtons();
+    }
+    private void setupTextViews() {
+        TextView t = (TextView) findViewById(R.id.results_latest_textview);
+        String[] tests = getResources().getStringArray(R.array.tests);
+        String[] result = tests[tests.length-1].split(";");
+        t.setText("Seneste resultat: " + result[2]);
+        t = (TextView) findViewById(R.id.results_latest_suggest_textview);
+        t.setText("Ændring i hørelse: " + result[4]);
+        t = (TextView) findViewById(R.id.results_latest_change_textview);
+        t.setText("Forslag: " + result[3]);
+    }
+    private void setupButtons() {
+        Button btn = (Button) findViewById(R.id.result_latest_earlier_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent mainIntent = new Intent(getApplicationContext(), EarlierResultsActivity.class);
+                ResultsActivity.this.startActivity(mainIntent);
+                ResultsActivity.this.finish();
+            }
+        });
     }
 }
